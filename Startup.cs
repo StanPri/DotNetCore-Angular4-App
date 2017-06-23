fca3de001d4aa16f2de860bfcbbdbc789171c513
StanPri <spribytkovsky@yahoo.com>
2017-06-23 11:32:34 -0700

    EF setup done. ConnectionString fixed

--- Startup.cs
+++ Startup.cs
@@ -30,7 +30,7 @@ namespace WebApplicationBasic
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:Default"]));
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default"))); //Configuration.GetConnectionString("Default");
             // Add framework services.
             services.AddMvc();
         }
