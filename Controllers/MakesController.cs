08175337bdc4d99db5dab11a54c485e3fb9b281b
StanPri <spribytkovsky@yahoo.com>
2017-06-26 16:53:59 -0700

    Building API

---
+++ Controllers/MakesController.cs
@@ -0,0 +1,26 @@
+﻿using App.Models;
+using App.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Controllers
+{
+    public class MakesController : Controller
+    {
+        private readonly AppDbContext context;
+        public MakesController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("/api/makes")]
+        public async Task<IEnumerable<Make>> GetMakes()
+        {
+            return await context.Makes.Include(m => m.Models).ToListAsync();
+        }
+    }
+}
